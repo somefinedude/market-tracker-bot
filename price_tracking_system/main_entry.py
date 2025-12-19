7f72e0479d8a87f97616a8ba2fa2d4ff79359667
Abdulaziz Komiljonov <abdulazizkomiljonov4621@gmail.com>
2025-12-19 13:19:07 +0500

    Add source code and requirements.txt, bot reads .env

--- price_tracking_system/main_entry.py
+++ price_tracking_system/main_entry.py
@@ -1,7 +1,8 @@
 import os
 from fetcher import MarketFetcher
 from bot import MarketBot
-
+from dotenv import load_dotenv
+load_dotenv()
 fetcher = MarketFetcher(
     exchange_api_key=os.getenv("EXCHANGE_API_KEY")
 )
@@ -11,4 +12,6 @@ bot = MarketBot(
     fetcher=fetcher,
 )
 
+print("TELEGRAM:", os.getenv("TELEGRAM_BOT_TOKEN"))
+print("EXCHANGE:", os.getenv("EXCHANGE_API_KEY"))
 bot.run()
