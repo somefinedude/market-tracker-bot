9c94bd183b988652f89b0fd2625981072e39f3f1
Abdulaziz Komiljonov <abdulazizkomiljonov4621@gmail.com>
2025-12-20 18:56:17 +0500

    Added button

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ class MarketBot:
             [InlineKeyboardButton("USD / UZS", callback_data="usduzs")],
             [InlineKeyboardButton("USD / AUD", callback_data="usdaud")],
             [InlineKeyboardButton("USD / GBP", callback_data="usdgbp")],
-            [InlineKeyboardButton("🌍 Custom pair rate", callback_data="custompair")],
+            [InlineKeyboardButton("🌍 Custom pair", callback_data="custom_pair_info")],
             [InlineKeyboardButton("🔙 Go back", callback_data="market")],
         ]
 
